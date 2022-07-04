261016f854ee53fc3e4390bd0bb036e4128c1593
uselessexe <brezgin2001@gmail.com>
2022-07-04 18:55:40 +0300

    ок

--- front/angular-tour-of-heroes/src/app/app.component.spec.ts
+++ front/angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+//что это такое?
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
